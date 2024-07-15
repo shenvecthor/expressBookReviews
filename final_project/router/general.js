470092d520d7fae369b6b1631115536910b2c5a6
shenvecthor <140363918+shenvecthor@users.noreply.github.com>
2024-07-15 08:58:45 -0400

    Update general.js
    added book review functionality

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -58,8 +58,15 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+    const book = Object.values(books).find(b => b.isbn === isbn);
+
+    if (book) {
+        res.json(book.reviews);
+    } else {
+        res.status(404).json({ message: 'Book not found' });
+    }
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
