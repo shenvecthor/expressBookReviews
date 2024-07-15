c28b69580039631f8750e565e476e585003a764c
shenvecthor <140363918+shenvecthor@users.noreply.github.com>
2024-07-15 08:35:55 -0400

    get books by title - commit 3
    get books by title functionality added.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,8 +45,15 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+    const booksByTitle = Object.values(books).filter(b => b.title === title);
+
+    if (booksByTitle.length > 0) {
+        res.json(booksByTitle);
+    } else {
+        res.status(404).json({ message: 'Books with this title not found' });
+    }
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
